9e5520222c959d637241337232b030c908298651
valts08 <valts.pilans@gmail.com>
2025-04-28 22:20:02 +0300

    added cart component and its styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,20 @@
 import CompanyLogoImage from "../assets/images/logo.svg"
 import CartImage from "../assets/images/icon-cart.svg"
 import UserAvatar from "../assets/images/image-avatar.png"
+import { useState } from "react"
+import { createPortal } from "react-dom"
+import Cart from "./Cart"
 
 const NavBar = () => {
 
+    const [showCart, setShowCart] = useState(false)
+
+    const toggleShowCart = () => {
+        setShowCart(prevState => !prevState)
+    }
+
     return (
-        <nav className="flex justify-between h-[100px] border-b-1 border-neutral-100">
+        <nav className="flex justify-between h-[100px] border-b-1 border-neutral-100 relative">
             <section className="flex place-items-center">
                 <img src={CompanyLogoImage} alt="company-logo" className="h-[20px]" />
                     <ul className="flex lg:gap-6 md:gap-4 ml-[3em]">
@@ -17,9 +26,10 @@ const NavBar = () => {
                     </ul>
             </section>
             <section className="flex place-items-center">
-                <img src={CartImage} alt="cart-icon" className="h-[20px] w-[22px] mr-[3em] hover:cursor-pointer" />
+                <img src={CartImage} onClick={() => toggleShowCart()} alt="cart-icon" className="h-[20px] w-[22px] mr-[3em] hover:cursor-pointer" />
                 <img src={UserAvatar} alt="user-avatar" className="w-[50px] h-[50px] rounded-lg" />
             </section>
+            {showCart && createPortal(<Cart />, document.body)}
         </nav>
     )
 }
