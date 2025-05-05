2fe917eeca11db8374cd32bb4606ac695d48382c
valts08 <valts.pilans@gmail.com>
2025-05-05 22:53:24 +0300

    done with positioning of all modal elements

--- src/components/product-images/PictureModal.tsx
+++ src/components/product-images/PictureModal.tsx
@@ -14,26 +14,26 @@ const PictureModal = () => {
     }
 
     const handleSwitchImgPrevious = () => {
-        console.log(images.mainImageId % images.mainImages.length)
         dispatch(prevImage())
     }
 
     const handleSwitchImgNext = () => {
-        console.log(images.mainImageId % images.mainImages.length)
         dispatch(nextImage())
     }
 
     return (
-        <div className="absolute flex flex-col">
-            <img className="h-[20px] w-[20px] pb-3 cursor-pointer" src={CloseModalIcon} alt="close-modal" onClick={() => handleModalClose()}/>
-            <img className="h-[550px] w-[550px] border-0 rounded-lg" src={images.mainImages.at(images.mainImageId % images.mainImages.length)} alt="modal-image"/>
-            <div className="flex place-items-center h-[56px] w-[56px] border-1 bg-white rounded-full cursor-pointer" onClick={() => handleSwitchImgNext()}>
-                <img className="h-[16px] w-[12px] m-auto" src={NextImgIcon} alt="next-img-icon"/>
+        <div className="absolute flex flex-col top-[10dvh] left-[36dvw] mx-auto">
+            <img className="h-[20px] w-[20px] pb-3 cursor-pointer self-end" src={CloseModalIcon} alt="close-modal" onClick={() => handleModalClose()}/>
+            <img className="h-[550px] w-[550px] border-0 rounded-lg aspect-square" src={images.mainImages.at(images.mainImageId % images.mainImages.length)} alt="modal-image"/>
+            <div className="flex absolute mt-[17em]">
+                <div className="flex translate-x-[-1.75em] place-items-center h-[56px] w-[56px] border-0 bg-white rounded-full  cursor-pointer" onClick={() => handleSwitchImgPrevious()}>
+                    <img className="h-[16px] w-[12px] m-auto" src={PrevImgIcon} alt="previous-img-icon"/>
+                </div>
+                <div className="flex ml-[27.35em] translate-x-[1.75em] place-items-center h-[56px] w-[56px] border-0 bg-white rounded-full cursor-pointer" onClick={() => handleSwitchImgNext()}>
+                    <img className="h-[16px] w-[12px] m-auto" src={NextImgIcon} alt="next-img-icon"/>
+                </div>
             </div>
-            <div className="flex place-items-center h-[56px] w-[56px] border-1 bg-white rounded-full  cursor-pointer" onClick={() => handleSwitchImgPrevious()}>
-                <img className="h-[16px] w-[12px] m-auto" src={PrevImgIcon} alt="previous-img-icon"/>
-            </div>
-            <div className="flex justify-between pt-[2em]">
+            <div className="flex justify-evenly pt-[2em]">
                 {images.thumbnails && images.thumbnails.map((thumbnail, i) => {
                     return <ThumbnailImg key={i} image={thumbnail} number={i}/>
                 })}
