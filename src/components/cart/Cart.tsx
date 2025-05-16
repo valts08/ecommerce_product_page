66411464c7ef49553b100ee543232172a199f10b
valts08 <valts.pilans@gmail.com>
2025-05-16 18:50:44 +0300

    cart element positioning now is fully dynamic for all intended screen sizes

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -3,10 +3,10 @@ import CartItemList from "./CartItemList"
 
 const Cart = () => {
     const products = useAppSelector(state => state.products)
-    const listIsEmpty = products.length == 0 
+    const listIsEmpty = products.length == 0
 
     return (
-        <div className="flex flex-col justify-evenly absolute border-0 bg-white rounded-lg min-h-[256px] max-w-[360px] top-[5.25em] lg:right-[12vw] md:right-[1em] w-100 shadow-lg shadow-black/30">
+        <div className={`flex flex-col justify-evenly absolute border-0 bg-white rounded-lg min-h-[256px] sm:left-auto left-[50%] sm:top-[5.25em] top-[27.5%] w-100 translate-x-[-50%] translate-y-[-50%] sm:translate-none sm:right-[1rem] md:right-[1rem] lg:right-[12vw] shadow-lg shadow-black/30`}>
             <header className="py-[1em] pl-3 font-semibold">Cart</header>
             <div className="border-b-1 border-neutral-100"></div>
             {listIsEmpty ? (<span className="text-neutral-400 font-semibold m-auto">Your cart is empty.</span>)
